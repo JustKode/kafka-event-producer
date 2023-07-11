3a3dca581bbe6c7f5eebbac52dbc29408761e2e4
JustKode <sobu0715@gmail.com>
2023-07-12 00:48:01 +0900

    feat: Add StringMeta Class

---
+++ src/main/java/justkode/kafka/event/producer/meta/StringMeta.java
@@ -0,0 +1,23 @@
+package justkode.kafka.event.producer.meta;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class StringMeta {
+    private String name;
+    private Boolean isRandomString;
+    private List<String> manualStrings;
+    private Integer minLength;
+    private Integer maxLength;
+
+    private boolean isValid() {
+        if (isRandomString) {
+            return minLength <= maxLength && minLength >= 0;
+        } else {
+            return manualStrings != null;
+        }
+    }
+}
