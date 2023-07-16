473c3c0636bfed9f46f5f565698c9ad81e01845b
JustKode <sobu0715@gmail.com>
2023-07-16 15:45:08 +0900

    feat: add isValid in DoubleMeta

--- src/main/java/justkode/kafka/event/producer/meta/DoubleMeta.java
+++ src/main/java/justkode/kafka/event/producer/meta/DoubleMeta.java
@@ -1,4 +1,25 @@
 package justkode.kafka.event.producer.meta;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+@AllArgsConstructor
 public class DoubleMeta {
+    private String name;
+    private Boolean isManual;
+    private List<Integer> manualIntegers;
+    private Double minValue;
+    private Double maxValue;
+
+    public Boolean isValid() {
+        if (!isManual) {
+            return minValue <= maxValue;
+        } else {
+            return manualIntegers != null;
+        }
+    }
 }
